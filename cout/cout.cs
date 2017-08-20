6acc7b076f49a519ae4ac463b015c6913ec903e4
hernani Aleman Ferraz <afhernani@hotmail.com>
2017-08-20 01:07:54 +0100

    move cout and testcout

---
+++ cout/cout.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: hernani
+ * Fecha: 20/08/2017
+ * Hora: 1:04
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace syscout
+{
+	/// <summary>
+	/// Description of cout.
+	/// </summary>
+	public class cout
+	{
+		public cout()
+		{
+		}
+	}
+}

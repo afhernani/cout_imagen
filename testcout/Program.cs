807adffb7684fd8e0eb0cfb0f34b6e2feebc1bc6
hernani Aleman Ferraz <afhernani@hotmail.com>
2017-08-20 01:01:50 +0100

    add project cout and testcout

---
+++ testcout/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: hernani
+ * Fecha: 20/08/2017
+ * Hora: 0:59
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace testcout
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("test cout!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
